bb181114dfc854eb16e1205406ea31ac8230b096
boldCoder <gaurav.developer@outlook.com>
2023-08-14 15:07:42 +0530

    added new function

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	consolePrint()
+	consolePrint1()
 }
 
 func consolePrint() {
 	fmt.Println("Hi! This is Print Function")
+}
+
+func consolePrint1() {
+	fmt.Println("Hi! This is Print1 Function")
 }
